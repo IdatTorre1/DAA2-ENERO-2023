a3158dce4664ce496e5859079d01bcd5a46bc4e0
alexis5999 <alexis5999@gmail.com>
2023-02-16 20:58:24 -0500

    nueva pelicula

---
+++ src/main/java/idat/edu/pe/daa2/repositorios/PeliculaRepositorio.java
@@ -0,0 +1,21 @@
+package idat.edu.pe.daa2.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import idat.edu.pe.daa2.modelo.Pelicula;
+
+public interface PeliculaRepositorio extends CrudRepository<Pelicula, Integer>{
+
+	
+	@Query(value = "SELECT a FROM Pelicula a WHERE a.nombre =?1")
+	public List<Pelicula> buscarPeliculasPorNombre(String nombre);
+	
+	
+	@Query(value = "SELECT a FROM Pelicula a WHERE a.nombre like CONCAT(?1, '%')")
+     public List<Pelicula> buscarPeliculaLikeNombre(String nombre);
+	
+	
+}
